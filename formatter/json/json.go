9f6067289e96faf4267430823b8dde94755599d9
Pavel <pavel_iv37@mail.ru>
2026-04-29 15:22:09 +0300

    fix bug

--- formatter/json/json.go
+++ formatter/json/json.go
@@ -8,9 +8,14 @@ import (
 
 // Json форматирует дерево различий в стиле json
 func Json(nodes []comparefiles.Node) (string, error) {
-    data, err := json.MarshalIndent(nodes, "", "  ")
-    if err != nil {
-        return "", fmt.Errorf("failed to marshal JSON: %w", err)
-    }
-    return string(data), nil
+	// Оборачиваем массив в структуру с ключом "diff"
+	wrapper := map[string]any{
+		"diff": nodes,
+	}
+	
+	data, err := json.MarshalIndent(wrapper, "", "  ")
+	if err != nil {
+		return "", fmt.Errorf("failed to marshal JSON: %w", err)
+	}
+	return string(data), nil
 }
